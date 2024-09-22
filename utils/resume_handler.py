41dac2d8b242670e31637fbec609c92864375209
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-22 12:28:37 +0530

    add temp dir

--- utils/resume_handler.py
+++ utils/resume_handler.py
@@ -1,18 +1,28 @@
 import os
+from tempfile import TemporaryDirectory
 from langchain_community.document_loaders import Docx2txtLoader, TextLoader, PyPDFLoader
 
 
 def load_document(uploaded_document):
-    upload_document_extension=os.path.splitext(uploaded_document.name)[1]
-    
-    if upload_document_extension==".txt":
-        loader=TextLoader(uploaded_document.name)
-    elif upload_document_extension==".pdf":
-        loader=PyPDFLoader(uploaded_document.name)
-    elif upload_document_extension==".docx":
-        loader=Docx2txtLoader(uploaded_document.name)
-    
-    if loader:
-        resume_data=loader.load()
+    try:
+        temp_dir=TemporaryDirectory()
+        temp_file_path=os.path.join(temp_dir, uploaded_document.name)
         
-    return resume_data
+        with open(temp_file_path, "rb") as temp_file:
+            temp_file.write(uploaded_document.read())
+        
+        upload_document_extension=os.path.splitext(uploaded_document.name)[1]
+        
+        if upload_document_extension==".txt":
+            loader=TextLoader(temp_file_path)
+        elif upload_document_extension==".pdf":
+            loader=PyPDFLoader(temp_file_path)
+        elif upload_document_extension==".docx":
+            loader=Docx2txtLoader(temp_file_path)
+        
+        if loader:
+            resume_data=loader.load()
+            
+        return resume_data
+    finally:
+        temp_dir.cleanup()
