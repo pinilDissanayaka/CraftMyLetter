51ba28458315058148a2ea8b85eaf45e3ac652ac
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-22 11:58:50 +0530

    change web based loader

--- utils/jobpost_handler.py
+++ utils/jobpost_handler.py
@@ -2,7 +2,7 @@ import os
 from langchain_community.document_loaders import WebBaseLoader
 
 
-def get_job_description(job_description=None, job_post_url=None):
+def get_job_description(job_description=None, job_post_url:str=None):
     if job_description:
         return job_description
     else:
